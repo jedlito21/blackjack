dae078611a0f8bad431f3112b0ee9a2dd5e0587e
Tomáš Jedlička <jedlito21@dst22814.sps-prosek.lad>
2023-01-16 11:26:44 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+import random
+
+def card_value():
+    for key, value in values.items():
+        print(value)
+
+#vytvořený balík karet
+rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+color = ["Hearts", "Diamonds", "Spades", "Clubs"]
+
+values = {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "Jack": 10, "Queen": 10, "King": 10, "Ace": 11}
+
+pack = [{"Value": rank, "Color": color} for hodnota in rank for barva in color]
+
+#dealování karet
+
+card_rank = random.choice(rank)
+card_color = random.choice(color)
+
+print(card_rank)
+print(card_color)
+
+card_value()
