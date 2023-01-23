7d60e837a0920c036540b17b9b7ce4f57113c6b4
jedlito21 <jedlito21@sps-prosek.cz>
2023-01-23 11:40:30 +0100

    přidány karty do ruky hráče a dealera

--- main.py
+++ main.py
@@ -1,36 +1,63 @@
 import random
 
 #vytvořený balík karet
-rank = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
-color = ["Hearts", "Diamonds", "Spades", "Clubs"]
+ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
+colors = ["Hearts", "Diamonds", "Spades", "Clubs"]
+pack = [{"Rank": rank, "Color": color} for rank in ranks for color in colors]
+random.shuffle(pack)
 
-#nastavení limitu a součtu
+card_count = 0
+#nastavení limitu a součtu a žetonu
 count = 0
 limit = 21
+chips = 1000
 
 #převod slov na hodnoty
-def value_of_card(card_rank):
-    if card_rank.isnumeric():
-        return int(card_rank)
-    elif card_rank == "Jack" or card_rank == "Queen" or card_rank == "King":
+def value_of_card(card):
+    if card.isnumeric():
+        return int(card)
+    elif card == "Jack" or card == "Queen" or card == "King":
         return 10
-    elif card_rank == "Ace":
+    elif card == "Ace":
         if count > limit:
             return 1
         else:
             return 11
 
+def deal_card(pack):
+    card = pack.pop()
+    player_cards.append(card)
 
 
-pack = [{"Value": rank, "Color": color} for hodnota in rank for barva in color]
+#ukládání karet
 
-#dealování karet
-
-card_rank = random.choice(rank)
-card_color = random.choice(color)
+player_cards = []
+dealer_cards = []
 
 
-print(card_rank)
-print(card_color)
+gameloop = True
 
-print(value_of_card(card_rank))
+#while gameloop == True:
+
+#první tah
+
+if card_count == 0:
+    deal_card(pack)
+    card_count = card_count + 1
+    card = pack.pop()
+    dealer_cards.append(card)
+if card_count == 1:
+    deal_card(pack)
+    card_count = card_count + 1
+    card = pack.pop()
+    dealer_cards.append(card)
+    print(player_cards)
+    print(dealer_cards)
+
+
+#druhý tah
+
+
+
+
+print(card_count)
