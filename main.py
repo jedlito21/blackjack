ce0c02b302ff8efd74a12c6e20b4c15adcb8d130
tomas <jedlito21@sps-prosek.cz>
2023-01-28 12:10:39 +0100

    Dodělat dealera, vyřešit gameloop, ošetřit vstupy a otestovat

--- main.py
+++ main.py
@@ -1,36 +1,41 @@
 import random
 
-#vytvořený balík karet
+# vytvořený balík karet
 ranks = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King", "Ace"]
 colors = ["Hearts", "Diamonds", "Spades", "Clubs"]
 pack = [{"Rank": rank, "Suit": color} for rank in ranks for color in colors]
 random.shuffle(pack)
 
 card_count = 0
-#nastavení limitu a součtu a žetonu
+card_count_dealer = 0
+# nastavení limitu a součtu a žetonu
 count = 0
+count_dealer = 0
 limit = 21
 chips = 1000
 
-#převod slov na hodnoty
+# převod slov na hodnoty
 def value_of_card(card):
     if card.isnumeric():
         return int(card)
     elif card == "Jack" or card == "Queen" or card == "King":
         return 10
     elif card == "Ace":
-        if count > limit:
+        if count + 11 > limit:
             return 1
         else:
             return 11
 
-def deal_card(pack):
+def deal_card_player(pack):
     card = pack.pop()
     player_cards.append(card)
 
+def deal_card_dealer(pack):
+    card = pack.pop()
+    dealer_cards.append(card)
 
 
-#ukládání karet
+# ukládání karet
 
 player_cards = []
 dealer_cards = []
@@ -38,33 +43,68 @@ dealer_cards = []
 
 gameloop = True
 
-#while gameloop == True:
+while gameloop == True:
 
-#první tah
+    # první tah
+    if card_count == 0:
+        deal_card_dealer(pack)
+        deal_card_player(pack)
+        card_count = card_count + 1
+        card_count_dealer = card_count_dealer + 1
+        count = count + value_of_card(player_cards[0]['Rank'])
+    if card_count == 1:
+        deal_card_dealer(pack)
+        deal_card_player(pack)
+        card_count = card_count + 1
+        card_count_dealer = card_count_dealer + 1
+        print("__________________________________", "\n", "Your cards: ", player_cards[0]['Rank'], player_cards[0]['Suit'], "|", player_cards[1]['Rank'], player_cards[1]['Suit'])
+        print("Dealer cards: ", dealer_cards[0]['Rank'], dealer_cards[0]['Suit'], "| Unknown")
+        count = count + value_of_card(player_cards[1]['Rank'])
+        print("Your sum: ", count, "\n", "________________________________")
+        if count == 21:
+            print("BLACKJACK!")
+            break
+    # druhý tah
 
-if card_count == 0:
-    deal_card(pack)
-    card_count = card_count + 1
-    card = pack.pop()
-    dealer_cards.append(card)
-    count = count + value_of_card(card['Rank'])
-if card_count == 1:
-    deal_card(pack)
-    card_count = card_count + 1
-    card = pack.pop()
-    dealer_cards.append(card)
-    print("__________________________________", "\n", "Your cards: ", player_cards[0]['Rank'], player_cards[0]['Suit'], "|", player_cards[1]['Rank'], player_cards[1]['Suit'])
-    print("Dealer cards: ", dealer_cards[0]['Rank'], dealer_cards[0]['Suit'], "| Unknown")
-    count = count + value_of_card(card['Rank'])
-    print("Your sum: ", count, "\n", "________________________________")
-#druhý tah
-if input("Do you want to take a card?  1 - yes / 2 - no") == "1":
-    pass
-elif input("Do you want to take a card?  1 - yes / 2 - no") == "2":
-    pass
-else:
-    print("You need to pick 1 or 2")
+    take_card = input("Do you want to take a card?  1 - yes / 2 - no")
 
-# print(card_count)
+    if take_card == "1":
+        if card_count == 2:
+            deal_card_player(pack)
+            card_count = card_count + 1
+            print("__________________________________", "\n", "Your cards: ", player_cards[0]['Rank'], player_cards[0]['Suit'], "|", player_cards[1]['Rank'], player_cards[1]['Suit'], "|", player_cards[2]['Rank'], player_cards[2]['Suit'])
+            print("Dealer cards: ", dealer_cards[0]['Rank'], dealer_cards[0]['Suit'], "| Unknown")
+            count = count + value_of_card(player_cards[2]['Rank'])
+            print("Your sum: ", count, "\n", "________________________________")
+            if count > 21:
+                print("You are over limit!")
+                break
+            elif count < 21:
+                take_card = input("Do you want to take a card?  1 - yes / 2 - no")
+                if take_card == "1":
+                    if card_count == 3:
+                        deal_card_player(pack)
+                        card_count = card_count + 1
+                        print("__________________________________", "\n", "Your cards: ", player_cards[0]['Rank'], player_cards[0]['Suit'], "|", player_cards[1]['Rank'], player_cards[1]['Suit'], "|", player_cards[2]['Rank'], player_cards[2]['Suit'], "|", player_cards[3]['Rank'], player_cards[3]['Suit'])
+                        print("Dealer cards: ", dealer_cards[0]['Rank'], dealer_cards[0]['Suit'], "| Unknown")
+                        count = count + value_of_card(player_cards[3]['Rank'])
+                        print("Your sum: ", count, "\n", "________________________________")
+                        if count > 21:
+                            print("You are over limit!")
+                            break
+                        elif count < 21:
+                            take_card = input("Do you want to take a card?  1 - yes / 2 - no")
+                        else:
+                            print("YOU WON!")
+                elif take_card == "2":
+                    break
+            else:
+                print("YOU WON!")
+    elif take_card == "2":
 
+        break
+    else:
+        print("You need to pick 1 - yes or 2 - no")
+        break
 
+    # print(card_count)
